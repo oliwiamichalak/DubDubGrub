0af4e2ed78d474da0db06868964bcc35e6f4b28d
oliwiamichalak <oliwia.michalak@gmail.com>
2022-03-18 13:51:38 +0000

    feat: List view

--- DubDubGrub/DubDubGrub/LocationListView.swift
+++ DubDubGrub/DubDubGrub/LocationListView.swift
@@ -9,12 +9,51 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("List!")
+        NavigationView {
+            List {
+                ForEach(0..<10) { item in
+                    HStack {
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+
+                        VStack(alignment: .leading) {
+                            Text("Test Location Name Long")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+
+                            HStack {
+                                AvatartView()
+                                AvatartView()
+                                AvatartView()
+                            }
+                        }
+                        .padding(.leading, 8)
+                    }
+                }
+            }
+            .navigationTitle("Grub Spots")
+        }
     }
 }
 
 struct LocationListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationListView()
+    }
+}
+
+struct AvatartView: View {
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35)
+            .clipShape(Circle())
     }
 }
