ed86bd2ada78ba0f150b9272d0ad769dc5cd2980
oliwiamichalak <oliwia.michalak@gmail.com>
2022-04-29 14:49:14 +0100

    feat: Constants

---
+++ DubDubGrub/DubDubGrub/Utilties/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DubDubGrub
+//
+//  Created by Oliwia Michalak on 29/04/2022.
+//
+
+import Foundation
+
+enum RecordType {
+    static let location = "DDGLocation"
+    static let profile  = "DDGProfile"
+}
