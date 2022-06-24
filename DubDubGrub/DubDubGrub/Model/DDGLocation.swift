d30e96b9c4536ac6de39269007479519fc53e67e
oliwiamichalak <oliwia.michalak@gmail.com>
2022-06-24 15:27:44 +0100

    refactor: formatting to match iCloud database

--- DubDubGrub/DubDubGrub/Model/DDGLocation.swift
+++ DubDubGrub/DubDubGrub/Model/DDGLocation.swift
@@ -8,13 +8,13 @@
 import CloudKit
 
 struct DDGLocation {
-    static let kName        = "name"
+    static let kName = "name"
     static let kDescription = "description"
     static let kSquareAsset = "squareAsset"
     static let kBannerAsset = "bannerAsset"
-    static let kAddress     = "address"
-    static let kLocation    = "location"
-    static let kWebsiteURL  = "websiteURL"
+    static let kAddress = "address"
+    static let kLocation = "location"
+    static let kWebsiteURL = "websiteURL"
     static let kPhoneNumber = "phoneNumber"
     
     let ckRecordID: CKRecord.ID
@@ -28,14 +28,14 @@ struct DDGLocation {
     let phoneNumber: String
     
     init(record: CKRecord) {
-        ckRecordID  = record.recordID
-        name        = record[DDGLocation.kName] as? String ?? "N/A"
+        ckRecordID = record.recordID
+        name = record[DDGLocation.kName] as? String ?? "N/A"
         description = record[DDGLocation.kDescription] as? String ?? "N/A"
         squareAsset = record[DDGLocation.kSquareAsset] as? CKAsset
         bannerAsset = record[DDGLocation.kBannerAsset] as? CKAsset
-        address     = record[DDGLocation.kAddress] as? String ?? "N/A"
-        location    = record[DDGLocation.kLocation] as? CLLocation ?? CLLocation(latitude: 0, longitude: 0)
-        websiteURL  = record[DDGLocation.kWebsiteURL] as? String ?? "N/A"
+        address = record[DDGLocation.kAddress] as? String ?? "N/A"
+        location = record[DDGLocation.kLocation] as? CLLocation ?? CLLocation(latitude: 0, longitude: 0)
+        websiteURL = record[DDGLocation.kWebsiteURL] as? String ?? "N/A"
         phoneNumber = record[DDGLocation.kPhoneNumber] as? String ?? "N/A"
     }
 }
--- DubDubGrub/DubDubGrub/Model/DDGProfile.swift
+++ DubDubGrub/DubDubGrub/Model/DDGProfile.swift
@@ -8,11 +8,11 @@
 import CloudKit
 
 struct DDGProfile {
-    static let kFirstName   = "firstName"
-    static let kLastName    = "lastName"
-    static let kAvatar      = "avatar"
+    static let kFirstName = "firstName"
+    static let kLastName = "lastName"
+    static let kAvatar = "avatar"
     static let kCompanyName = "companyName"
-    static let kBio         = "bio"
+    static let kBio = "bio"
     static let kIsCheckedIn = "isCheckedIn"
     
     let ckRecordID: CKRecord.ID
@@ -24,11 +24,11 @@ struct DDGProfile {
     let isCheckedIn: CKRecord.Reference? = nil
     
     init(record: CKRecord) {
-        ckRecordID  = record.recordID
-        firstName   = record[DDGProfile.kFirstName] as? String ?? "N/A"
-        lastName    = record[DDGProfile.kLastName] as? String ?? "N/A"
-        avatar      = record[DDGProfile.kAvatar] as? CKAsset
+        ckRecordID = record.recordID
+        firstName = record[DDGProfile.kFirstName] as? String ?? "N/A"
+        lastName = record[DDGProfile.kLastName] as? String ?? "N/A"
+        avatar = record[DDGProfile.kAvatar] as? CKAsset
         companyName = record[DDGProfile.kCompanyName] as? String ?? "N/A"
-        bio         = record[DDGProfile.kBio] as? String ?? "N/A"
+        bio = record[DDGProfile.kBio] as? String ?? "N/A"
     }
 }
